51f307cc6d53ed6d7baa9e749d31c5c2dac9487e
Matheus Reichert <matheus_ernan@outlook.com>
2026-05-02 19:54:05 -0300

    style(frontend): remove overlay and scrollbars from auth card

--- frontend/src/components/organisms/AuthBanner/AuthBanner.tsx
+++ frontend/src/components/organisms/AuthBanner/AuthBanner.tsx
@@ -1,21 +1,21 @@
-import { Logo } from "../../atoms/Logo/Logo";
+import { Logo } from '../../atoms/Logo/Logo';
 
 interface AuthBannerProps {
-	imageSrc: string;
+  imageSrc: string;
 }
 
 export function AuthBanner({ imageSrc }: AuthBannerProps) {
-	return (
-		<div className="relative w-full h-full min-h-[300px] lg:min-h-0 bg-bg-base overflow-hidden flex items-end">
-			<img
-				src={imageSrc}
-				alt=""
-				className="absolute inset-0 w-full h-full object-cover"
-			/>
-			<div className="absolute inset-0 bg-black/40" />
-			<div className="relative z-10 p-10 w-full flex justify-center">
-				<Logo variant="small" />
-			</div>
-		</div>
-	);
+  return (
+    <div className="relative w-full h-full min-h-[300px] lg:min-h-0 bg-bg-base overflow-hidden flex items-end">
+      <img
+        src={imageSrc}
+        alt=""
+        className="absolute inset-0 w-full h-full object-cover"
+      />
+      {/* Removed the black overlay div */}
+      <div className="relative z-10 p-10 w-full flex justify-center">
+        <Logo variant="small" />
+      </div>
+    </div>
+  );
 }
